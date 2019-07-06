f84516c22c1684356087d387eedcf029029f18b7
Glenn Paquette <glenn@atomicgrowth.co>
2019-07-05 21:40:42 -0400

    Trying another wkhtmltopdf file

--- generate.py
+++ generate.py
@@ -13,7 +13,7 @@ heroku = Heroku(app)
 
 print('loading wkhtmltopdf path on localhost')
 MYDIR = os.path.dirname(__file__)
-WKHTMLTOPDF_CMD = os.path.join(MYDIR + "/bin/", "./wkhtmltopdf")
+WKHTMLTOPDF_CMD = os.path.join(MYDIR + "/vendor/wkhtmltox/lib/", "libwkhtmltox.so")
 pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
 
